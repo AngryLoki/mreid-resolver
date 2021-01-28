b8ba6f292ce4e7463750cee6ae66af0288637644
AngryLoki <AngryLoki@users.noreply.github.com>
2021-01-28 16:59:48 +0300

    Fix iframe url

--- src/GoogleSearch.svelte
+++ src/GoogleSearch.svelte
@@ -4,24 +4,22 @@
     export let kgid: string;
     export let main_lang: string;
 
-    $: src =
-        "https://www.google.com/search?" +
-        stringifyUrl({
-            url: "https://www.google.com/search",
-            query: {
-                prvw: "AP1HcD1TvBAuAZZqq5dYyslxOHow",
-                kgmid: kgid,
-                hl: main_lang,
-                gl: "US",
-                q: "previewid:5a5c1869-ef41-46c2-a38d-1b2dbd3234a4",
-                useragent:
-                    "Mozilla/5.0 (iPhone; CPU iPhone OS 11_1 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) GSA/39.0.175034278 Mobile/15B93 Safari/604.1",
-                filter: "0",
-                gws_rd: "cr",
-                newwindow: "1",
-                igu: "1",
-            },
-        });
+    $: src = stringifyUrl({
+        url: "https://www.google.com/search",
+        query: {
+            prvw: "AP1HcD1TvBAuAZZqq5dYyslxOHow",
+            kgmid: kgid,
+            hl: main_lang,
+            gl: "US",
+            q: "previewid:5a5c1869-ef41-46c2-a38d-1b2dbd3234a4",
+            useragent:
+                "Mozilla/5.0 (iPhone; CPU iPhone OS 11_1 like Mac OS X) AppleWebKit/604.1.34 (KHTML, like Gecko) GSA/39.0.175034278 Mobile/15B93 Safari/604.1",
+            filter: "0",
+            gws_rd: "cr",
+            newwindow: "1",
+            igu: "1",
+        },
+    });
 </script>
 
 <iframe
@@ -30,6 +28,7 @@
     title="Search for {kgid} in Google"
     {src}
 />
+
 <style>
     #search-frame {
         height: 100%;
